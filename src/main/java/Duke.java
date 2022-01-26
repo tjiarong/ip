38ef2a2803bd2875ebf71ad6342391b429b2933a
tjiarong <jia8991@gmail.com>
2022-01-26 23:46:48 +0800

    Added Duke level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,10 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         String line = "";
         Scanner in = new Scanner(System.in);
-        String[] items = new String[100];
+        Task[] items = new Task[100];
         int itemCount = 0;
 
         String logo = "_______  _______ _________ _______ _________ _______  _\n"
@@ -24,22 +23,45 @@ public class Duke {
                 "____________________________________________________________\n"
                 );
 
+        line = in.nextLine();
         while (!line.equals("bye")){
-            line = in.nextLine();
-            if (line.equals("list")){
-                System.out.println("____________________________________________________________\n");
-                for (int i = 1; i <= itemCount; i++){
-                    System.out.println(i  + ". " + items[i - 1]);
+            if (line.equals("list")) {
+                System.out.println("____________________________________________________________\n" +
+                        "   Here are the tasks in your list:\n");
+                for (int i = 1; i <= itemCount; i++) {
+                    System.out.println(i + ".[" + items[i - 1].getStatusIcon() + "] " + items[i - 1].description);
                 }
                 System.out.println("____________________________________________________________\n");
+            } else if (line.contains("unmark")){
+                int taskNum = Integer.parseInt(line.substring(line.length() - 1));
+                if (taskNum <= itemCount){
+                    System.out.println("____________________________________________________________\n" +
+                            "OK, I've marked this task as not done yet:\n" +
+                            "[] " + items[taskNum - 1].description + "\n" +
+                            "____________________________________________________________\n"
+                    );
+                    items[taskNum - 1].markAsNotDone();
+                }
+            } else if (line.contains("mark")) {
+                int taskNum = Integer.parseInt(line.substring(line.length() - 1));
+                if (taskNum <= itemCount) {
+                    System.out.println("____________________________________________________________\n" +
+                            "Nice! I've marked this task as done:\n" +
+                            "[X] " + items[taskNum - 1].description + "\n" +
+                            "____________________________________________________________\n"
+                    );
+                    items[taskNum - 1].markAsDone();
+                }
             } else{
-                items[itemCount] = line;
+                Task t = new Task(line);
+                items[itemCount] = t;
                 itemCount++;
                 System.out.println("____________________________________________________________\n" +
                         "added: " + line + "\n" +
                         "____________________________________________________________\n"
                 );
             }
+            line = in.nextLine();
         }
 
         System.out.println("____________________________________________________________\n" +
