1aacf1e621e581be3a69fe7d7487bcd2d3821d04
tjiarong <jia8991@gmail.com>
2022-01-27 07:54:53 +0800

    Updated tags and minor changes

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,8 +3,10 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String line;
+        final int MAX_TASK = 100;
+
         Scanner in = new Scanner(System.in);
-        Task[] items = new Task[100];
+        Task[] items = new Task[MAX_TASK];
         int itemCount = 0;
         int taskNum = 0;
 
@@ -17,58 +19,58 @@ public class Duke {
                     + "| )      | (___) |   | |   | )   ( |   | |   | (___) || )  \\  | \n"
                     + "|/       (_______)   )_(   |/     \\|   )_(   (_______)|/    )_) \n";
 
-        System.out.println("____________________________________________________________\n" +
-                logo + "\n" +
-                "Hello! I'm Duke\n" +
-                "What can I do for you?\n" +
-                "____________________________________________________________\n"
+        System.out.println("_____________________________________________\n"
+                        + logo + "\n"
+                        + "Hello! I'm Duke\n"
+                        + "What can I do for you?\n"
+                        + "_____________________________________________\n"
                 );
 
         line = in.nextLine();
-        while (!line.equals("bye")){
+        while (!line.equals("bye")) {
             if (line.equals("list")) {
-                System.out.println("____________________________________________________________\n" +
-                        "   Here are the tasks in your list:\n");
+                System.out.println("_____________________________________________\n"
+                        + "   Here are the tasks in your list:\n");
                 for (int i = 1; i <= itemCount; i++) {
                     System.out.println(i + ".[" + items[i - 1].getStatusIcon() + "] "
                             + items[i - 1].description);
                 }
-                System.out.println("____________________________________________________________\n");
-            } else if (line.contains("unmark")){
+                System.out.println("_____________________________________________\n");
+            } else if (line.contains("unmark")) {
                 taskNum = Integer.parseInt(line.substring(line.length() - 1));
-                if (taskNum <= itemCount){
-                    System.out.println("____________________________________________________________\n" +
-                            "OK, I've marked this task as not done yet:\n" +
-                            "[] " + items[taskNum - 1].description + "\n" +
-                            "____________________________________________________________\n"
+                if (taskNum <= itemCount) {
+                    System.out.println("_____________________________________________\n"
+                            + "OK, I've marked this task as not done yet:\n"
+                            + "[] " + items[taskNum - 1].description + "\n"
+                            + "_____________________________________________\n"
                     );
                     items[taskNum - 1].markAsNotDone();
                 }
             } else if (line.contains("mark")) {
                 taskNum = Integer.parseInt(line.substring(line.length() - 1));
                 if (taskNum <= itemCount) {
-                    System.out.println("____________________________________________________________\n" +
-                            "Nice! I've marked this task as done:\n" +
-                            "[X] " + items[taskNum - 1].description + "\n" +
-                            "____________________________________________________________\n"
+                    System.out.println("_____________________________________________\n"
+                            + "Nice! I've marked this task as done:\n"
+                            + "[X] " + items[taskNum - 1].description + "\n"
+                            + "_____________________________________________\n"
                     );
                     items[taskNum - 1].markAsDone();
                 }
-            } else{
+            } else {
                 Task t = new Task(line);
                 items[itemCount] = t;
                 itemCount++;
-                System.out.println("____________________________________________________________\n" +
-                        "added: " + line + "\n" +
-                        "____________________________________________________________\n"
+                System.out.println("_____________________________________________\n"
+                        + "added: " + line + "\n"
+                        + "_____________________________________________\n"
                 );
             }
             line = in.nextLine();
         }
 
-        System.out.println("____________________________________________________________\n" +
-                "Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n"
+        System.out.println("_____________________________________________\n"
+                        + "Bye. Hope to see you again soon!\n"
+                        + "_____________________________________________\n"
                 );
     }
 }
